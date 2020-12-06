d942f076f0102505e6b9a6f55948055dda4747dd
Mikey Donohue <mwdonohue1234@gmail.com>
2020-12-05 20:58:12 -0500

    Loaded JSON Data and added drop-down menu

--- covid-19-visualizer/src/App.js
+++ covid-19-visualizer/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
 function App() {
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => {
+    const getJsonData = async () => {
+      fetch("https://api.covidtracking.com/v1/states/current.json")
+        .then((response) => response.json())
+        .then((data) => {
+          setJsonData(data);
+        });
+    };
+    getJsonData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID-19 Visualizer</h1>
+      {["state"].map((key) => (
+        <select key={key}>
+          {jsonData.map(({ [key]: value }) => (
+            <option key={value}>{value}</option>
+          ))}
+        </select>
+      ))}
     </div>
   );
 }
