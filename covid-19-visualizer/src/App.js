34509d442ecf71d0edfd788d98b4402db6c43047
Mikey Donohue <mwdonohue1234@gmail.com>
2020-12-08 03:10:02 -0500

    Added menu and table for selected state

--- covid-19-visualizer/src/App.js
+++ covid-19-visualizer/src/App.js
@@ -1,28 +1,50 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 function App() {
-  const [jsonData, setJsonData] = useState([]);
-
-  useEffect(() => {
-    const getJsonData = async () => {
-      fetch("https://api.covidtracking.com/v1/states/current.json")
-        .then((response) => response.json())
-        .then((data) => {
-          setJsonData(data);
-        });
-    };
-    getJsonData();
-  }, []);
+  const [covidStates, setCovidStates] = useState([]);
+  const [stateData, setStateData] = useState({});
+  useEffect(() =>
+    fetch("https://api.covidtracking.com/v1/states/current.json")
+      .then((response) => response.json())
+      .then((data) => setCovidStates(data))
+  );
+  useEffect(() => {}, [stateData]);
+  const onLandMassChange = (e) => {
+    setStateData(covidStates[e.target.value]);
+  };
   return (
     <div className="App">
       <h1>COVID-19 Visualizer</h1>
-      {["state"].map((key) => (
-        <select key={key}>
-          {jsonData.map(({ [key]: value }) => (
-            <option key={value}>{value}</option>
-          ))}
-        </select>
-      ))}
+      <select onChange={onLandMassChange}>
+        <option>Select a state</option>
+        {covidStates.map((entry, index) => (
+          <option key={index} value={index}>
+            {entry.state}
+          </option>
+        ))}
+      </select>
+      <div>
+        {(() => {
+          if (stateData !== undefined && Object.keys(stateData).length !== 0) {
+            return (
+              <div className="table">
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Deaths:</th>
+                      <td>{stateData.death}</td>
+                    </tr>
+                    <tr>
+                      <th>Death Increase:</th>
+                      <td>{stateData.deathIncrease}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            );
+          }
+        })()}
+      </div>
     </div>
   );
 }
