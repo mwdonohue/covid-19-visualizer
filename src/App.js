4f86d15ef61c067778bc07a6bda7aa04a03bda15
Mikey Donohue <mwdonohue1234@gmail.com>
2020-12-23 22:19:30 -0500

    Made the width of the graph 100%

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function App() {
         {(() => {
           if (graphData !== undefined && stateData !== undefined) {
             return (
-              <ResponsiveContainer width="95%" height={400}>
+              <ResponsiveContainer width="100%" height={400}>
                 <LineChart data={graphData}>
                   <Line type="monotone" dataKey="inc" stroke="#8884d8" />
                   <CartesianGrid stroke="#ccc" />
