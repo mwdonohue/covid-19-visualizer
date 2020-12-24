99e7fe20260877d29c210a80512eef664908b7c1
Mikey Donohue <mwdonohue1234@gmail.com>
2020-12-23 20:54:19 -0500

    Did some styling, obliterated two huge bugs

--- src/App.js
+++ src/App.js
@@ -1,50 +1,66 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import { LineChart, XAxis, YAxis, CartesianGrid, Line } from "recharts";
+import {
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Line,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 function App() {
   const [covidStates, setCovidStates] = useState([]);
   const [stateData, setStateData] = useState(undefined);
   const [graphData, setGraphData] = useState(undefined);
-
-  useEffect(() =>
-    fetch("https://api.covidtracking.com/v1/states/current.json")
-      .then((response) => response.json())
-      .then((data) => setCovidStates(data))
+  useEffect(
+    () =>
+      fetch("https://api.covidtracking.com/v1/states/current.json")
+        .then((response) => response.json())
+        .then((data) => setCovidStates(data)),
+    []
   );
   useEffect(() => {
     if (stateData !== undefined) {
       let currDate = new Date();
-      let graphData = [];
-      for (let i = 0; i <= 29; i++) {
-        currDate.setDate(currDate.getDate() - 1);
-        let dateString = (() => {
-          return (
-            currDate.getFullYear() +
-            "" +
-            getProperNum(currDate.getMonth() + 1) +
-            "" +
-            getProperNum(currDate.getDate())
+      let graphDataPromises = [];
+      let gData = [];
+      (async () => {
+        for (let i = 0; i <= 29; i++) {
+          currDate.setDate(currDate.getDate() - 1);
+          let dateString = (() => {
+            return (
+              currDate.getFullYear() +
+              "" +
+              getProperNum(currDate.getMonth() + 1) +
+              "" +
+              getProperNum(currDate.getDate())
+            );
+          })();
+          graphDataPromises.unshift(
+            fetch(
+              "https://api.covidtracking.com/v1/states/" +
+                stateData.state +
+                "/" +
+                dateString +
+                ".json"
+            ).then((response) => response.json())
           );
-        })();
-        fetch(
-          "https://api.covidtracking.com/v1/states/" +
-            stateData.state +
-            "/" +
-            dateString +
-            ".json"
-        )
-          .then((response) => response.json())
-          .then((data) =>
-            graphData.unshift({
+        }
+        await Promise.all(graphDataPromises).then((data) => {
+          // console.log(data);
+          data.forEach((element) => {
+            gData.push({
               name:
-                dateString.substring(4, 6) + "/" + dateString.substring(6, 8),
-              uv: data.positiveIncrease,
-              pv: Math.random(),
-              amt: Math.random(),
-            })
-          );
-      }
-      setGraphData(graphData);
+                element.date.toString().substring(4, 6) +
+                "/" +
+                element.date.toString().substring(6, 8),
+              inc: element.positiveIncrease,
+            });
+          });
+          setGraphData(gData);
+        });
+      })();
     }
   }, [stateData]);
 
@@ -62,7 +78,7 @@ function App() {
     <div className="App">
       <h1 className="title">COVID-19 Visualizer</h1>
       <select className="selectButton" onChange={onLandMassChange}>
-        <option>Select a state</option>
+        <option>Select state</option>
         {covidStates.map((entry, index) => (
           <option key={index} value={index}>
             {entry.state}
@@ -71,7 +87,7 @@ function App() {
       </select>
       <div className="table">
         {(() => {
-          if (stateData !== undefined) {
+          if (graphData !== undefined && stateData !== undefined) {
             return (
               <div>
                 <table>
@@ -85,7 +101,7 @@ function App() {
                       <td>{stateData.deathIncrease}</td>
                     </tr>
                     <tr>
-                      <th>Number Positive Increase:</th>
+                      <th>Today's Positive Increase:</th>
                       <td>{stateData.positiveIncrease}</td>
                     </tr>
                   </tbody>
@@ -95,16 +111,20 @@ function App() {
           }
         })()}
       </div>
+
       <div className="graph">
         {(() => {
-          if (graphData !== undefined && graphData.length === 30) {
+          if (graphData !== undefined && stateData !== undefined) {
             return (
-              <LineChart width={600} height={300} data={graphData}>
-                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey="name" />
-                <YAxis />
-              </LineChart>
+              <ResponsiveContainer width="95%" height={400}>
+                <LineChart data={graphData}>
+                  <Line type="monotone" dataKey="inc" stroke="#8884d8" />
+                  <CartesianGrid stroke="#ccc" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip></Tooltip>
+                </LineChart>
+              </ResponsiveContainer>
             );
           }
         })()}
